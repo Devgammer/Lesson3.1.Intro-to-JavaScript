fe42b8b796821bc0efad831efd85b03bea0764fc
“Dmitry <DKolokhin@gmail.com>
2025-03-31 15:08:33 +0300

    The hometask is done

---
+++ JsBasics1/Task2.js
@@ -0,0 +1,8 @@
+//Конкатенация
+//const name = 'Dima';
+//const surname = 'Kolokhin';
+//const fullName = name + " " + surname;
+//console.log(fullName);
+
+//let fullNameTemplate = `${name} ${surname}`;
+//console.log(fullNameTemplate);
---
+++ JsBasics1/task1.js
@@ -0,0 +1,28 @@
+//numbers
+//let x = 1;
+//let y = 2;
+//console.log(x + y);
+
+//undefined
+//let Age;
+//const name = 'Dima Kolokhin';
+//console.log(typeof age);
+
+//string
+//let Age = 20;
+//const name = 'Dima Kolokhin';
+//console.log(name);
+
+//Boolean
+//let isUserLogged = true;
+//let isUserOut = false;
+//console.log(isUserOut);
+
+//NaN
+//let number = 6 / 'Dima Kolokhin';
+//сonsole.log(number);
+
+//null
+//let day = null;
+//сonsole.log(day);
+
