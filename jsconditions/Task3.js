1d619c2999725391cba42dd55e85764602bee23f
“Dmitry <DKolokhin@gmail.com>
2025-04-05 16:54:49 +0300

    done

---
+++ jsconditions/Task3.js
@@ -0,0 +1,17 @@
+// Цикл for
+// const number = 7;
+// for (let i = 1; i <= 10; i++) {
+//     let result = number * i;
+//     console.log(`${number}x${i} = ${result}`);  
+//     }
+
+
+
+// const number = 7;
+// let i = 1;
+
+// while (i <= 10) {
+//     let result = number * i;
+//     console.log(`${number}x${i} = ${result}`);
+//     i++;
+// }
---
+++ jsconditions/task1.js
@@ -0,0 +1,14 @@
+// const averageGrade = 100;
+// let message;
+// if  (averageGrade <= 59){
+//     message = 'Незадовільно';
+// } else if (averageGrade <= 70){
+//     message = 'Задовільно';
+// } else if (averageGrade <= 80){
+//     message = 'Добре';
+// } else if (averageGrade <= 90){
+//     message = 'Дуже добре';
+// } else if (averageGrade <= 100){
+//     message = 'Відмінно';
+// } 
+// console.log(message);
